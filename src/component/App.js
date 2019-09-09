2b5db79ea7d954ca080bccfaf6646c476baa8316
Marcela Llanos <marcela3llanos@gmail.com>
2019-09-09 17:16:06 -0400

    Creacion del componente Header

---
+++ src/component/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+import '../css/App.css';
+
+function App() {
+  return (
+		<div className = "App container">
+			<Header title = "Gasto semanal"/>
+		</div>
+  );
+}
+
+export default App;
