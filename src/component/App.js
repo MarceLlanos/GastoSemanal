dd92b309ecfa1d712c6c2728d8c468e3fededa72
Marcela Llanos <marcela3llanos@gmail.com>
2019-09-09 17:58:16 -0400

    Creacion de formulario parte de la vista

--- src/component/App.js
+++ src/component/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Header from './Header';
+import Formulario from './Formulario';
 import '../css/App.css';
 
 function App() {
   return (
 		<div className = "App container">
 			<Header title = "Gasto semanal"/>
-			<div className = "row">
-			<div className ="one-half column"></div>
-			<div className ="one-half column"></div>
-		</div>
+			<div className ="contenido-principal contenido">
+				<div className = "row">
+					<div className ="one-half column">
+						<Formulario/>
+					</div>
+					<div className ="one-half column"></div>
+				</div>
+			</div>
 		</div>
   );
 }
